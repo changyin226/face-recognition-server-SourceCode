a3eff8883d1f2e065d8482c1c0efac03fa08ed59
changyin <changyin226@gmail.com>
2021-08-14 21:48:17 +0800

    增加驗證

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -4,6 +4,10 @@ const requireAuth = (redisClient) => (req, res, next) => {
   authorization = authorization.replace('Bearer ', '')
   redisClient.get(authorization, (err, reply) => {
     if (err || !reply) return res.status(401).json('授權過期，請重新登入')
+    const { id } = req.params
+    if(reply !== id) {
+      return res.status(401).json('該Token不能查找此筆資料')
+    }
     return next()
   })
 }
